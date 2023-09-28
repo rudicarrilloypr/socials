da004d6aa61c37fc9d39c0b20a0e1915676fe757
rudicarrilloypr <125106965+rudicarrilloypr@users.noreply.github.com>
2023-09-28 10:46:59 -0600

    Fix linters

--- spec/factories/comments.rb
+++ spec/factories/comments.rb
@@ -1,10 +1,9 @@
 # spec/factories/comments.rb
 
 FactoryBot.define do
-    factory :comment do
-      text { "Sample Comment Content" }
-      association :post
-      association :author, factory: :user
-    end
+  factory :comment do
+    text { 'Sample Comment Content' }
+    association :post
+    association :author, factory: :user
   end
-  
+end
