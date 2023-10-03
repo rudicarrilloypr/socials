cde792efe142f1db2762b24276c2cda8f7664e15
rudicarrilloypr <125106965+rudicarrilloypr@users.noreply.github.com>
2023-10-03 09:57:25 -0600

    Fix rubocop offenses

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,13 +1,12 @@
 class PostsController < ApplicationController
-    def index
-      @user = User.find(params[:user_id])
-      @posts = @user.posts
-    end
-  
-    def show
-      @user = User.find(params[:user_id])
-      @post = @user.posts.find(params[:id])
-      @recent_comments = @post.five_recent_comments
-    end
+  def index
+    @user = User.find(params[:user_id])
+    @posts = @user.posts
   end
-  
+
+  def show
+    @user = User.find(params[:user_id])
+    @post = @user.posts.find(params[:id])
+    @recent_comments = @post.five_recent_comments
+  end
+end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,10 @@
 class UsersController < ApplicationController
-    def index
-      @users = User.all
-    end
-  
-    def show
-      @user = User.find(params[:id])
-      @recent_posts = @user.three_recent_posts
-    end
+  def index
+    @users = User.all
   end
-  
+
+  def show
+    @user = User.find(params[:id])
+    @recent_posts = @user.three_recent_posts
+  end
+end
