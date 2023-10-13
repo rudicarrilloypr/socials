9ebb1f59f030624f9d36b02b3dc2346d84a7e5d5
rudicarrilloypr <rudicarrilloypr@gmail.com>
2023-10-13 11:38:03 -0600

    Fix offenses

--- app/models/post.rb
+++ app/models/post.rb
@@ -10,7 +10,7 @@ class Post < ApplicationRecord
   def five_recent_comments
     comments.order(created_at: :desc).limit(5)
   end
-  
+
   validates :title, presence: true, length: { maximum: 250 }
   validates :text, presence: true
   validates :comments_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
